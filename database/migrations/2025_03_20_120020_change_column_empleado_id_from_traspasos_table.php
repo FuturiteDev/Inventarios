98deafd90b8b9d652279e0b80ca58cf974131b71
Alejandro Vargas <122814856+alejandro-vargas-m@users.noreply.github.com>
2025-03-20 13:51:20 -0600

    fix

--- database/migrations/2025_03_20_120020_change_column_empleado_id_from_traspasos_table.php
+++ database/migrations/2025_03_20_120020_change_column_empleado_id_from_traspasos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeColumnEmpleadoIDFromTraspasosTable extends Migration
+class ChangeColumnEmpleadoIdFromTraspasosTable extends Migration
 {
     /**
      * Run the migrations.
